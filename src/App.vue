26f9ee65cf39f2a30dc8fce8e23eb94affccda9c
ergofly <supersha520@gmail.com>
2022-11-08 19:42:22 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TicTacToe></TicTacToe>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TicTacToe from "@/components/TicTacToe";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TicTacToe
   }
 }
 </script>
 
 <style>
-#app {
+html,body,#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 </style>
